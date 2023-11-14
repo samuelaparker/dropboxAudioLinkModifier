5850ce873f3efcc1d6a730130f8551a2f3b337b3
Samuel Parker <samuel.a.parker00@gmail.com>
2023-11-13 20:53:06 -0500

    first commit

---
+++ dropboxAudioLinkModifier.js
@@ -0,0 +1,21 @@
+function modifyDropboxLink(originalLink) {
+  // Replace "&amp;" with "&"
+  const correctedLink = originalLink.replace(/&amp;/g, "&");
+
+  // Check if the link contains the "dl=0" parameter and hasn't been modified
+  if (correctedLink.includes("dl=0") && !correctedLink.includes("dl=1")) {
+    // Modify the link to set "dl=1"
+    return correctedLink.replace("dl=0", "dl=1");
+  }
+  // If "dl=0" is not found or it has already been modified, return the corrected link
+  return correctedLink;
+}
+
+// Replace the shared link with your Dropbox shared link
+const dropboxSharedLink = "YOUR_DROPBOX_URL_HERE";
+// Modify the Dropbox link
+const modifiedLink = modifyDropboxLink(dropboxSharedLink);
+
+// Set the modified link as the source for the audio element
+const audioElement = document.getElementById("audioPlayer");
+audioElement.src = modifiedLink;
